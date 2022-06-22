076755027cf23542f83fc77f7e69546554a74f85
Menanya <emmmiemenz@gmail.com>
2022-06-22 22:14:50 +0100

    temp

--- src/index.js
+++ src/index.js
@@ -5,11 +5,32 @@ import pokemonCard from './modules/pokemonCard.js';
 
 const pokedex = 157;
 
+
+
 const getPokemon = async () =>{
   for (let i = 1; i < pokedex; i += 1) {
     const id = [i];
     pokemonCard(id);
   }
+  const likebtn = document.querySelectorAll(".btnlike");
+likebtn.forEach(btn =>{
+  btn.addEventListener("click", addlike)
+})
 }
 
 getPokemon();
+
+
+function addlike(e){
+  for(let i=1; i<=pokedex; i++){
+    if(e.target.id === `lk${i}`){
+      console.log("clicked")
+        }
+  }
+  
+
+// console.log("yea")
+
+}
+
+// e.target.parentNode.className
--- src/modules/display.js
+++ src/modules/display.js
@@ -6,7 +6,7 @@ function display(pokemon) {
   divCol.innerHTML = `
     <img src=${pokemon.sprites.front_default}>
     
-    <div class="top"><h3>${pokemon.name}</h3><span> <i class="like fa-solid fa-heart"></i> </span> </div>
+    <div class="top"><h3>${pokemon.name}</h3><span id="lk${pokemon.id}" class="btnlike"> <i class="like fa-solid fa-heart"></i> </span> </div>
     <button> Comment</button>
     <button> Reservation</button>
 
